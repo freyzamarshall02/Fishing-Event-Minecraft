cb9978229c8eeb2bec198ab3e0882077215e0de5
FreyzaMarshall <134063936+freyzamarshall02@users.noreply.github.com>
2025-08-14 15:46:39 +0700

    Update FishingStartCommand.java

--- src/main/java/com/iwak/fishing/FishingStartCommand.java
+++ src/main/java/com/iwak/fishing/FishingStartCommand.java
@@ -5,9 +5,12 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class StartCommand implements CommandExecutor {
+public class FishingStartCommand implements CommandExecutor {
     private final FishingManager manager;
-    public StartCommand(FishingManager manager) { this.manager = manager; }
+
+    public FishingStartCommand(FishingManager manager) {
+        this.manager = manager;
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -15,20 +18,25 @@ public class StartCommand implements CommandExecutor {
             sender.sendMessage(ChatColor.RED + "No permission.");
             return true;
         }
+
         if (args.length != 1) {
             sender.sendMessage(ChatColor.YELLOW + "Usage: /" + label + " <seconds>");
             return true;
         }
+
         int sec;
-        try { sec = Integer.parseInt(args[0]); }
-        catch (NumberFormatException e) {
+        try {
+            sec = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
             sender.sendMessage(ChatColor.RED + "Not a number: " + args[0]);
             return true;
         }
+
         if (sec <= 0) {
             sender.sendMessage(ChatColor.RED + "Seconds must be > 0");
             return true;
         }
+
         manager.start(sec);
         sender.sendMessage(ChatColor.GREEN + "Fishing event started for " + sec + " seconds.");
         return true;

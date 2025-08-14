3af124df06d6075a1586ced9bc0a4de1be39c452
FreyzaMarshall <134063936+freyzamarshall02@users.noreply.github.com>
2025-08-14 14:34:18 +0700

    Create FishingPlaceholders.java

---
+++ src/main/java/com/iwak/fishing/FishingPlaceholders.java
@@ -0,0 +1,40 @@
+package com.iwak.fishing;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.NotNull;
+
+public class FishingPlaceholders extends PlaceholderExpansion {
+
+    private final FishingManager manager;
+
+    public FishingPlaceholders(FishingManager manager) {
+        this.manager = manager;
+    }
+
+    @Override public @NotNull String getIdentifier() { return "fishing_event"; }
+    @Override public @NotNull String getAuthor() { return "iwak"; }
+    @Override public @NotNull String getVersion() { return "1.0.0"; }
+    @Override public boolean persist() { return true; }
+
+    @Override
+    public String onRequest(OfflinePlayer player, @NotNull String params) {
+        // Supported:
+        // 1_name .. 5_name
+        // 1_score .. 5_score
+        switch (params.toLowerCase()) {
+            case "1_name": return manager.getTopName(1);
+            case "2_name": return manager.getTopName(2);
+            case "3_name": return manager.getTopName(3);
+            case "4_name": return manager.getTopName(4);
+            case "5_name": return manager.getTopName(5);
+
+            case "1_score": return manager.getTopScore(1);
+            case "2_score": return manager.getTopScore(2);
+            case "3_score": return manager.getTopScore(3);
+            case "4_score": return manager.getTopScore(4);
+            case "5_score": return manager.getTopScore(5);
+        }
+        return "";
+    }
+}

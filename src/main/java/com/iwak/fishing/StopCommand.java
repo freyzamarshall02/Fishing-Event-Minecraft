5ca7b818c36a369f438d15fe3fcfcaf7ab70fef3
FreyzaMarshall <134063936+freyzamarshall02@users.noreply.github.com>
2025-08-14 14:34:52 +0700

    Create StopCommand.java

---
+++ src/main/java/com/iwak/fishing/StopCommand.java
@@ -0,0 +1,23 @@
+package com.iwak.fishing;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class StopCommand implements CommandExecutor {
+    private final FishingManager manager;
+    public StopCommand(FishingManager manager) { this.manager = manager; }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!sender.hasPermission("fishingevent.admin")) {
+            sender.sendMessage(ChatColor.RED + "No permission.");
+            return true;
+        }
+        boolean announce = true; // announce winners on manual stop too
+        manager.forceStop(announce);
+        sender.sendMessage(ChatColor.GREEN + "Fishing event stopped.");
+        return true;
+    }
+}

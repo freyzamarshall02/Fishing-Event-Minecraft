40a5d424eba447cedcdc1bd3dfe6f5d2c96ec6e1
FreyzaMarshall <134063936+freyzamarshall02@users.noreply.github.com>
2025-08-17 10:15:42 +0700

    Create FishingResetCommand.java

---
+++ src/main/java/com/iwak/fishing/FishingResetCommand.java
@@ -0,0 +1,20 @@
+package com.iwak.fishing;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class FishingResetCommand implements CommandExecutor {
+
+    private final FishingManager manager;
+
+    public FishingResetCommand(FishingManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        manager.resetLeaderboard();
+        return true;
+    }
+}
